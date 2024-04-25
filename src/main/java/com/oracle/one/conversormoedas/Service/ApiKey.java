0217075e70e99cf2eaea09218c6c48a1f2471122
valeriasouza28 <82419677+valeriasouza28@users.noreply.github.com>
2024-04-25 18:39:54 -0300

    Update ApiKey.java

--- src/main/java/com/oracle/one/conversormoedas/Service/ApiKey.java
+++ src/main/java/com/oracle/one/conversormoedas/Service/ApiKey.java
@@ -3,8 +3,5 @@ package com.oracle.one.conversormoedas.Service;
 public class ApiKey {
     private  String apiKey;
 
-    public String getApiKey() {
-        this.apiKey = "c0a1742c2c9a2a612ce58a45";
-        return apiKey;
-    }
+    
 }
